b80b924ab101d306f28e49c168ae6651cdf9d7c5
Felype <joaofelype015@gmail.com>
2025-07-28 16:32:12 -0300

    login page (3/12)

--- lib/ui/screens/login_page.dart
+++ lib/ui/screens/login_page.dart
@@ -1,17 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:engenharia_de_software/constants/colorsConstants.dart';
+import 'package:engenharia_de_software/constants/imagesConstants.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPage();
-}
-
-class _LoginPage extends State<LoginPage> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: AppColors.backgroundColor,
+      appBar: AppBar(
+        title: const Text(
+          'Login',
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Volta para a tela anterior
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(48.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset(
+                AppImages.busIcon,
+                height: 120,
+              ),
+              const SizedBox(height: 64),
+              TextField(
+                style: TextStyle(
+                  color: AppColors.lightText,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'E-mail ou usuário',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                obscureText: true,
+                style: TextStyle(
+                  color: AppColors.lightText,
+                ),
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  // ação de login
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.buttonColor,
+                  foregroundColor: AppColors.darkText,
+                  fixedSize: const Size(200, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: const Text(
+                  'Entrar',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
