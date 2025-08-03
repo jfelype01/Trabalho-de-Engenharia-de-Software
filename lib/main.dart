723757e6c3b47bafbbe96b906bbf573361f45624
Pedro Henrique Sabbi <pedrohenriquesabbi@gmail.com>
2025-08-03 20:30:54 -0300

    Conectando firebase
    instalação e alteração de arquivos importantes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 // lib/main.dart
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:flutter/material.dart';
 import 'login_screen.dart'; // Importa a tela de login
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
